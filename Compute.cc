636b353b0a9285a16c1d7b405bd8efedd1155cbf
YoungjoMin <tuna10516@kaist.ac.kr>
2019-03-28 22:44:12 +0900

    EvalHelper coefficient problem fix

--- Compute.cc
+++ Compute.cc
@@ -1,32 +1,48 @@
 # include "iRRAM.h"
-# include <functional>
+# include "./ANALYTIC.h"
 
 using namespace iRRAM;
 
-COMPLEX (*Tcoef)(int);
-int Tk = 1;
-int Td = 1;
+COMPLEX seq(int k) {
+	static REAL prevSeq;
+	static int prevK;
+	REAL a(1);
+	int i=2;
 
+	if(k<=1)
+		return COMPLEX(1);
+
+	if(prevK<k) {
+		a = prevSeq;
+		i = prevK+1;
+	}
+	for(;i<=k;i++)
+			a/=i;
+
+	prevSeq = a;
+	prevK = k;
+	return COMPLEX(a);
+}
+/*
 COMPLEX seq(int k) {
 	REAL a(1);
 	if(k<=1)
 		return COMPLEX(1);
-
 	for(int i=2;i<=k;i++)
-			a/=i;
+		a/=i;
 	return COMPLEX(a);
 }
+*/
 
 COMPLEX evalHelper(int p, COMPLEX (*coef)(int), const COMPLEX& z, int k, int d) {
-
-	COMPLEX result = coef(0);
-	COMPLEX pow = z;
+	COMPLEX result(0);
+	COMPLEX pow(1);
 	COMPLEX cur;
 	INTEGER a;
 	int t = k-p + 32*d;
-	for(int i = 1; i<=t;i++) {
-		a=INTEGER(1);
-		cur = coef(i)*pow;
+
+	for(int i = 0; i<=t;i++) {
+		cur = coef(i+d)*pow;
 		for(int j=d+i;j>i;j--) {
 			a=a*INTEGER(j);
 			cur = cur*COMPLEX(j);
@@ -38,24 +54,23 @@ COMPLEX evalHelper(int p, COMPLEX (*coef)(int), const COMPLEX& z, int k, int d) 
 	return result;
 }
 
+COMPLEX eval(COMPLEX(*coef)(int), const COMPLEX& z, int k, int d) {
+	
+	static COMPLEX (*Tcoef)(int) = coef;
+	static int Tk = k;
+	static int Td = d;
+	
+	COMPLEX(*lambda)(int, const COMPLEX&)  =  ([] (int p, const COMPLEX& z) -> COMPLEX {
+			return evalHelper(p,Tcoef,z,Tk,Td); });
 
-
-
-
-COMPLEX eval(int p,const COMPLEX& z) {
-
-		
-		return evalHelper(p,Tcoef,z,Tk,Td); 
+	return limit( lambda , z);
 }
 
 void compute()
 {
-	Tcoef= seq;
-	Tk = 1;
-	Td = 0;
-	COMPLEX z=pi();
-	cout<<setRwidth(100);
-	COMPLEX result = limit(eval,z);
+	COMPLEX result;
+	cout<<setRwidth(50);
+	result = eval(seq,COMPLEX(REAL(0.5),pi()),1,50);
 	cout<<result._real<<" + "<<result._imag<<" i\n";
 	return;
 }
--- Makefile
+++ Makefile
@@ -1,5 +1,32 @@
 # Makefile for user_programs
 
-all:
-        g++ -std=c++11 -g -O2 -I/home/hyunwoo/iRRAM/installed/include -Xlinker -rpath -Xlinker /home/hyunwoo/iRRAM/installed/lib  Compute.cc  -L/home/hyunwoo/iRRAM/installed/lib -liRRAM -lmpfr -lgmp -lm -lpthread -o Compute
+prefix=/usr/local
+exec_prefix=/usr/local
 
+CC = gcc -std=c11
+CFLAGS = -g -O2 
+CPP = gcc -E
+CPPFLAGS =  -I${prefix}/include
+CXX = g++ -std=c++11
+CXXCPP = g++ -E -std=c++11
+CXXFLAGS = -g -O2
+LDFLAGS = -Xlinker -rpath -Xlinker ${exec_prefix}/lib
+LDLIBS =  -L${exec_prefix}/lib -liRRAM -lmpfr -lgmp -lm -lpthread
+
+SOURCE = Compute ANAYTIC
+OBJECT = Compute.o ANALYTIC.o
+
+.PHONY: clean
+all:	Link
+
+Link: $(OBJECT)
+	$(CXX) $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS) $(OBJECT) $(LDLIBS)
+
+ANALYTIC.o: ANALYTIC.cc ANALYTIC.h
+	$(CXX) $(CXXFLAGS) -c $(CPPFLAGS) $(LDFLAGS) ANALYTIC.cc $(LDLIBS)
+
+Compute.o: Compute.cc
+	$(CXX) $(CXXFLAGS) -c $(CPPFLAGS) $(LDFLAGS) Compute.cc $(LDLIBS)
+
+clean:
+	for i in *.o ; do rm -vf `basename $$i .c` ; done;
